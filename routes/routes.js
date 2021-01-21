7010cc60132470a08c82d8cf5d716d99fa48f0d5
Michele Fitzgerald-Winick <mfitzwinick@gmail.com>
2021-01-20 16:30:15 -0800

    adding put and post routes

--- routes/routes.js
+++ routes/routes.js
@@ -3,7 +3,22 @@ const path = require("path");
 const Workout = require("../models/workout.js");
 
 router.get("/exercise", (req, res) => {
-    res.sendFile(path.join(__dirname, ".../public/exercise.html"));
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
+
+router.post("/api/workouts", function (req, res) {
+    Workout.create({})
+        .then(updatedExercise => {
+            res.json(updatedExercise)
+        })
+})
+
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+    console.log(params)
+    Workout.findByIdAndUpdate(params.id, { $push: { "exercises": body } })
+        .then(updatedExercise => {
+            res.json(updatedExercise)
+        })
 });
 
 router.get("/api/workouts", (req, res) => {
